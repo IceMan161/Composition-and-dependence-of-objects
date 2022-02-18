6d52c8cb65ab3fdca1e7b060c57ea378fbb94597
Artem Sukhanov <IceMan161@yandex.ru>
2022-02-18 13:17:55 +0300

    Find Last Test

--- src/main/java/ru/netology/domain/CartManager.java
+++ src/main/java/ru/netology/domain/CartManager.java
@@ -5,21 +5,22 @@ public class CartManager {
 
     private int maxValue = 10;
 
+    public CartManager() {
+        maxValue = 10;
+    }
+
+    public CartManager(int maxValue) {
+        this.maxValue = maxValue;
+    }
+
     public PurchaseFilm[] findAll() {
         return items;
     }
 
     public void add(PurchaseFilm item) {
-        // создаём новый массив размером на единицу больше
         int length = items.length + 1;
         PurchaseFilm[] tmp = new PurchaseFilm[length];
-        // itar + tab
-        // копируем поэлементно
-        // for (int i = 0; i < items.length; i++) {
-        //   tmp[i] = items[i];
-        // }
         System.arraycopy(items, 0, tmp, 0, items.length);
-        // кладём последним наш элемент
         int lastIndex = tmp.length - 1;
         tmp[lastIndex] = item;
         items = tmp;
@@ -27,36 +28,11 @@ public class CartManager {
 
     public PurchaseFilm[] getAll() {
         PurchaseFilm[] result = new PurchaseFilm[items.length];
-        // перебираем массив в прямом порядке
-        // но кладём в результаты в обратном
         for (int i = 0; i < result.length; i++) {
             int index = items.length - i - 1;
             result[i] = items[index];
         }
         return result;
-    }
-
-    // наивная реализация
-    public void removeById(int id) {
-        int length = items.length - 1;
-        PurchaseFilm[] tmp = new PurchaseFilm[length];
-        int index = 0;
-        for (PurchaseFilm item : items) {
-            if (item.getFilmId() != id) {
-                tmp[index] = item;
-                index++;
-            }
-        }
-        // меняем наши элементы
-        items = tmp;
-    }
-
-    public CartManager() {
-        maxValue = 10;
-    }
-
-    public CartManager(int maxValue) {
-        this.maxValue = maxValue;
     }
 
     public PurchaseFilm[] findLast() {
@@ -71,9 +47,6 @@ public class CartManager {
             int index = items.length - i - 1;
             result[i] = items[index];
         }
-
-        // заполняем result из массива что лежит в поле
-        // в обратном порядке
         return result;
     }
 }
--- src/test/java/ru/netology/domain/CartManagerTest.java
+++ src/test/java/ru/netology/domain/CartManagerTest.java
@@ -88,4 +88,30 @@ class CartManagerTest {
 
     }
 
+    @Test
+    public void shouldFindLastNoParam() {
+
+        CartManager manager = new CartManager();
+
+        PurchaseFilm first = new PurchaseFilm(1, "Avengers");
+        PurchaseFilm second = new PurchaseFilm(2, "Iron man");
+        PurchaseFilm third = new PurchaseFilm(3, "Avatar");
+        PurchaseFilm fourth = new PurchaseFilm(4, "Terminator");
+        PurchaseFilm fifth = new PurchaseFilm(5, "Thor");
+        PurchaseFilm sixth = new PurchaseFilm(6, "Hulk");
+        PurchaseFilm seventh = new PurchaseFilm(7, "Black Panther");
+
+        manager.add(first);
+        manager.add(second);
+        manager.add(third);
+        manager.add(fourth);
+        manager.add(fifth);
+        manager.add(sixth);
+        manager.add(seventh);
+
+        PurchaseFilm[] actual = manager.findLast();
+        PurchaseFilm[] expected = new PurchaseFilm[]{seventh, sixth, fifth, fourth, third, second, first};
+        assertArrayEquals(actual, expected);
+
+    }
 }
